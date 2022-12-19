8e5e224b5bab71e8ba6e03f0d18e2625a595694b
Treighton Mauldin <treightonmauldin@Treightons-MacBook-Pro-2.local>
2022-12-18 21:40:52 -0800

    added notification email

--- app/utils/email.server.tsx
+++ app/utils/email.server.tsx
@@ -8,6 +8,7 @@ const sendEmail = ({action, payload}) => {
     switch(action) {
         case 'CONFIRMATION':
             sendOrderConfirmationEmail(payload)
+            sendOrdernotification(payload)
             break
         default:
             console.error(`${action} does not exist`)
@@ -21,7 +22,6 @@ const sendOrderConfirmationEmail = async ({orderId, orderDate, order}) => {
         to: order.email, // Change to your recipient
         from: 'treighton@barbellfarm.com', // Change to your verified sender
         subject: `Order confirmation ${orderId} for ${order.orderType} on ${orderDate}`,
-        text: 'and easy to do anywhere, even with Node.js',
         html: `
         <h2> 
             Thanks for placing an order for ${order.orderType} ${ order.orderType === 'delivery' ? ` on ${ orderDate } between 10am and 1pm` : ''}.
@@ -37,6 +37,37 @@ const sendOrderConfirmationEmail = async ({orderId, orderDate, order}) => {
         </ul>
         <p className="mt-5">Your total will be $${order.total}.00.</p>
         <p>We accept Venmo, Cash, Check, and EBT, and you can at pick up!</p>
+        `,
+    }
+
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
+
+const sendOrdernotification = async ({orderId, orderDate, order}) => {
+    const items = order.items.map((item) => `<li>${item}</li>`).join('')
+    const msg = {
+        to: 'treighton@barbellfarm.com, brooke@helmstudio.co', // Change to your recipient
+        from: 'treighton@barbellfarm.com', // Change to your verified sender
+        subject: `Order confirmation ${orderId} for ${order.orderType} on ${orderDate}`,
+        text: 'and easy to do anywhere, even with Node.js',
+        html: `
+        <h2> 
+            ${order.name} Placed an order for ${order.orderType} ${ order.orderType === 'delivery' ? ` on ${ orderDate } between 10am and 1pm` : ''}.
+        </h2>
+        <p>
+            the items in their order are:
+        </p>
+        <ul>
+            ${items}
+        </ul>
+        <p className="mt-5">Your total will be $${order.total}.00.</p>
         `,
     }
 
